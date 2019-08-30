3bdc3f132088792076295e60dcd12c13fcdf1e81
Ruslan Murtazin <rouslanmurtazin@yandex.ru>
2019-08-31 01:12:21 +0300

    docker support

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+# Create publish artifact
+dotnet publish -c Release src
+
+# Build the Docker images
+docker build -t mruslan97/kfuapp:$TAG src/bin/Release/netcoreapp2.2/publish/.
+docker tag mruslan97/kfuapp:$TAG mruslan97/kfuapp:latest
+
+# Login to Docker Hub and upload images
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push mruslan97/kfuapp:$TAG
+docker push mruslan97/kfuapp:latest
