56b0a984e7cc96a9f461bba067d2ab5d94e3d19c
Ruslan Murtazin <rouslanmurtazin@yandex.ru>
2019-08-31 02:05:59 +0300

    config

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ cd ./Schedule/Schedule
 dotnet publish -c Release -o ./output
 
 # Build the Docker images
-docker build -t mruslan97/kfuapp:$TAG ./output/.
+docker build -t mruslan97/kfuapp:$TAG ./
 docker tag mruslan97/kfuapp:$TAG mruslan97/kfuapp:latest
 
 # Login to Docker Hub and upload images
