9a8e410438948f369e6436da46552c098e29b377
RuslanMurtazin <rouslanmurtazin@yandex.ru>
2019-08-31 03:27:53 +0300

    docker login

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -ev
 
-TAG=$1
-DOCKER_USERNAME=$2
-DOCKER_PASSWORD=$3
-
 # Build the Docker images
 chmod +x ./docker-build.sh
 ./docker-build.sh
 
 # Login to Docker Hub and upload images
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" --password-stdin
 docker push mruslan97/kfuapp:dev
