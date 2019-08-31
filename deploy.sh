42dbd2c7a389f12156ec0e304230c7702d915729
RuslanMurtazin <rouslanmurtazin@yandex.ru>
2019-08-31 03:40:09 +0300

    config

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ chmod +x ./docker-build.sh
 ./docker-build.sh
 
 # Login to Docker Hub and upload images
-docker login -u="${DOCKER_USERNAME}" --password-stdin="${DOCKER_PASSWORD}" 
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}" 
 docker push mruslan97/kfuapp:dev
