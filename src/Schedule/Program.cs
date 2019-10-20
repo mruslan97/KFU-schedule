acc16bfac09a70a5215b33ea0c7cb617e9a069fa
RuslanMurtazin <rouslanmurtazin@yandex.ru>
2019-10-20 18:56:36 +0300

    windows services support

--- src/Schedule/Program.cs
+++ src/Schedule/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.ServiceProcess;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
+using Schedule.Services;
 
 namespace Schedule
 {
@@ -15,7 +18,30 @@ namespace Schedule
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                Directory.SetCurrentDirectory(pathToContentRoot);
+            }
+
+            var builder = CreateWebHostBuilder(
+                args.Where(arg => arg != "--console").ToArray());
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                // To run the app without the CustomWebHostService change the
+                // next line to host.RunAsService();
+                host.RunAsCustomService();
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -23,8 +49,19 @@ namespace Schedule
                 .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Information);
                 })
                 .UseNLog()
+                .UseUrls("https://+:443")
                 .UseStartup<Startup>();
+    }
+    
+    public static class WebHostServiceExtensions
+    {
+        public static void RunAsCustomService(this IWebHost host)
+        {
+            var webHostService = new CustomWebHostService(host);
+            ServiceBase.Run(webHostService);
+        }
     }
 }
